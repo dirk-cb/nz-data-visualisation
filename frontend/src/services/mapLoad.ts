3c6e2c55d6642f18f6046e7ce23ed6de9423c432
dirk-cb <christopherbeestonnz@gmail.com>
2026-01-21 20:16:35 +0200

    Added fix

--- frontend/src/services/mapLoad.ts
+++ frontend/src/services/mapLoad.ts
@@ -1,19 +1,13 @@
 import { asyncBufferFromUrl, parquetReadObjects } from 'hyparquet';
 
 
-const BASE_PATH = "test"//import.meta.env.BASE_URL
-    //window.location.hostname === 'localhost' 
-   // || window.location.hostname === '127.0.0.1'? './' : (window.location.origin + import.meta.env.BASE_URL);
-
-console.log("test")
+const BASE_PATH = import.meta.env.BASE_URL
 
 const FILENAMES = ["region", "territorial", "sa3", "sa2"]
 
 const fetchCensusData = async (filename: string): Promise<any[]> => {
     
     const url = `${BASE_PATH}/data/${filename}.parquet`.replace(/\/+/g, '/');
-
-    console.log(url)
     
     const file = await asyncBufferFromUrl({ url });
     return await parquetReadObjects({ file });

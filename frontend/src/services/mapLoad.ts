b97af13bc513a124ad689ed07884744e76110be1
dirk-cb <christopherbeestonnz@gmail.com>
2026-01-21 21:40:00 +0200

    Remove old function

--- frontend/src/services/mapLoad.ts
+++ frontend/src/services/mapLoad.ts
@@ -1,21 +1,10 @@
-import { asyncBufferFromUrl, parquetMetadataAsync, parquetReadObjects } from 'hyparquet';
+import { asyncBufferFromUrl, parquetReadObjects } from 'hyparquet';
 
 
 const BASE_PATH = import.meta.env.BASE_URL
 
 const FILENAMES = ["region", "territorial", "sa3", "sa2"]
 
-async function loadParquet(url: string) {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    const file = {
-        byteLength: arrayBuffer.byteLength,
-        slice: (start: any, end: any) => arrayBuffer.slice(start, end)
-    };
- 
-    // 5. Now read the objects from the local "file" object
-    return file // await parquetReadObjects({ file });
-}
 
 const fetchCensusData = async (filename: string): Promise<any[]> => {
     
